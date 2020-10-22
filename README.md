2a50fd1b4cb99f4dd11e3d7f9e9eb03b7e65c854
manal-tarek <73298438+manal-tarek@users.noreply.github.com>
2020-10-22 16:59:10 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# landingPage
-landingPage
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+-This is the landing page project .
+- It contains 4 sections .
+-Each section has to be liked by a link .
+- I added some css to any li or ul lIKE backround-color .
+
---
+++ app.js
@@ -0,0 +1,74 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+   
+const edits = document.querySelectorAll("section");
+let fragment = document.createDocumentFragment();
+
+let   navList = document.querySelector("navbar__list");
+
+for(let i = 0; i<=4; i++){
+	 const addLi = document.createElement("li");
+   	 const addLink = document.createElement("a");
+	
+};
+
+
+
+
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
